cb152ff5d26ee655f6fa29a9ea646cc978ad95c2
matt venn <matt@mattvenn.net>
2016-04-22 20:11:41 +0200

    hacked but working

--- __init__.py
+++ __init__.py
@@ -1,61 +1,154 @@
+# -*- coding: utf-8 -*-
+
+__license__ = 'GPL 3'
+__copyright__ = '2009, John Schember <john@nachtimwald.com>'
+__docformat__ = 'restructuredtext en'
+
 import os
-from calibre.customize.conversion import OutputFormatPlugin
-
-from calibre.constants import get_version
-from calibre.customize.conversion import OutputFormatPlugin
-from calibre.ebooks.conversion import ConversionUserFeedBack
-from calibre.ebooks.conversion.plugins.epub_output import EPUBOutput
-from calibre.ebooks.oeb.base import OPF
-from calibre.utils.logging import Log
+import shutil
 
 
-class HelloWorldOutput(OutputFormatPlugin):
+from calibre.customize.conversion import OutputFormatPlugin, \
+    OptionRecommendation
+from calibre.ptempfile import TemporaryDirectory, TemporaryFile
 
-    name                = 'Hello World Output Plugin' # Name of the plugin
-    description         = 'blah '
-    supported_platforms = ['linux'] # Platforms this plugin will run on
-    author              = 'BBP' # The author of this plugin
-    version             = (1, 0, 0)   # The version number of this plugin
-    file_type           = 'bbp'
-#    file_types          = set(['epub', 'mobi']) # The file types that this plugin will be applied to
-#    on_postprocess      = True # Run this plugin after conversion is complete
-    minimum_calibre_version = (0, 7, 53)
+NEWLINE_TYPES = ['system', 'unix', 'old_mac', 'windows']
 
-    def __init__(self, *args, **kwargs):
-        self.epub_output_plugin = EPUBOutput(*args, **kwargs)
-        OutputFormatPlugin.__init__(self, *args, **kwargs)
-    
-    def convert(self, oeb_book, output, input_plugin, opts, log):
-        self.report_version(log)
+class TXTOutput(OutputFormatPlugin):
 
-        from calibre.rpdb import set_trace
-        set_trace()
+    name = 'BBP Output'
+    author = 'Matt Venn'
+    file_type = 'bbp'
 
+    options = set([
+        OptionRecommendation(name='newline', recommended_value='system',
+            level=OptionRecommendation.LOW,
+            short_switch='n', choices=NEWLINE_TYPES,
+            help=_('Type of newline to use. Options are %s. Default is \'system\'. '
+                'Use \'old_mac\' for compatibility with Mac OS 9 and earlier. '
+                'For Mac OS X use \'unix\'. \'system\' will default to the newline '
+                'type used by this OS.') % sorted(NEWLINE_TYPES)),
+        OptionRecommendation(name='txt_output_encoding', recommended_value='utf-8',
+            level=OptionRecommendation.LOW,
+            help=_('Specify the character encoding of the output document. ' \
+            'The default is utf-8.')),
+        OptionRecommendation(name='inline_toc',
+            recommended_value=False, level=OptionRecommendation.LOW,
+            help=_('Add Table of Contents to beginning of the book.')),
+        OptionRecommendation(name='max_line_length',
+            recommended_value=40, level=OptionRecommendation.LOW,
+            help=_('The maximum number of characters per line. This splits on '
+            'the first space before the specified value. If no space is found '
+            'the line will be broken at the space after and will exceed the '
+            'specified value. Also, there is a minimum of 25 characters. '
+            'Use 0 to disable line splitting.')),
+        OptionRecommendation(name='force_max_line_length',
+            recommended_value=True, level=OptionRecommendation.LOW,
+            help=_('Force splitting on the max-line-length value when no space '
+            'is present. Also allows max-line-length to be below the minimum')),
+        OptionRecommendation(name='txt_output_formatting',
+             recommended_value='plain',
+             choices=['plain', 'markdown', 'textile'],
+             help=_('Formatting used within the document.\n'
+                    '* plain: Produce plain text.\n'
+                    '* markdown: Produce Markdown formatted text.\n'
+                    '* textile: Produce Textile formatted text.')),
+        OptionRecommendation(name='keep_links',
+            recommended_value=False, level=OptionRecommendation.LOW,
+            help=_('Do not remove links within the document. This is only ' \
+            'useful when paired with a txt-output-formatting option that '
+            'is not none because links are always removed with plain text output.')),
+        OptionRecommendation(name='keep_image_references',
+            recommended_value=False, level=OptionRecommendation.LOW,
+            help=_('Do not remove image references within the document. This is only ' \
+            'useful when paired with a txt-output-formatting option that '
+            'is not none because links are always removed with plain text output.')),
+        OptionRecommendation(name='keep_color',
+            recommended_value=False, level=OptionRecommendation.LOW,
+            help=_('Do not remove font color from output. This is only useful when ' \
+                   'txt-output-formatting is set to textile. Textile is the only ' \
+                   'formatting that supports setting font color. If this option is ' \
+                   'not specified font color will not be set and default to the ' \
+                   'color displayed by the reader (generally this is black).')),
+     ])
+
+    def convert(self, oeb_book, output_path, input_plugin, opts, log):
         from calibre.ebooks.txt.txtml import TXTMLizer
         from calibre.utils.cleantext import clean_ascii_chars
         from calibre.ebooks.txt.newlines import specified_newlines, TxtNewlines
 
-        self.writer = TXTMLizer(log)
+        from calibre.rpdb import set_trace
+        set_trace()
+        opts.max_line_length = 40
+
+        if opts.txt_output_formatting.lower() == 'markdown':
+            from calibre.ebooks.txt.markdownml import MarkdownMLizer
+            self.writer = MarkdownMLizer(log)
+        elif opts.txt_output_formatting.lower() == 'textile':
+            from calibre.ebooks.txt.textileml import TextileMLizer
+            self.writer = TextileMLizer(log)
+        else:
+            self.writer = TXTMLizer(log)
 
         txt = self.writer.extract_content(oeb_book, opts)
         txt = clean_ascii_chars(txt)
-        from calibre import ipython
-        ipython(locals())
-        """ 
-        asin = None
-        for ident in oeb_book.metadata["identifier"]:
-            scheme = ident.get(OPF("scheme"), None).lower()
-            if (scheme.startswith("amazon") or scheme == "asin") and ident.value.startswith("B"):
-                asin = ident.value
-                break
-        
-        epub_filename = self.temporary_file(".epub").name
-        self.epub_output_plugin.convert(oeb_book, epub_filename, input_plugin, opts, log)  # convert input format to EPUB
-        log.info("Successfully converted input format to EPUB")
-        
-        self.convert_from_epub(JobLog(log), epub_filename, asin, output)
-        """ 
-    def report_version(self, log):
-        log.info("bbp output")
-        
 
+        log.debug('\tReplacing newlines with selected type...')
+        txt = specified_newlines(TxtNewlines(opts.newline).newline, txt)
+        txt = txt.encode(opts.txt_output_encoding, 'replace')
+        
+        from text_to_pef import get_pef
+        pef = get_pef(txt)
+
+        if not os.path.exists(os.path.dirname(output_path)) and os.path.dirname(output_path) != '':
+            os.makedirs(os.path.dirname(output_path))
+
+        import codecs
+        fh = codecs.open(output_path, "w", "utf-8")
+        #with open('out.pef','w') as fh:
+        fh.write(pef)
+
+
+class TXTZOutput(TXTOutput):
+
+    name = 'TXTZ Output'
+    author = 'John Schember'
+    file_type = 'txtz'
+
+    def convert(self, oeb_book, output_path, input_plugin, opts, log):
+        from calibre.ebooks.oeb.base import OEB_IMAGES
+        from calibre.utils.zipfile import ZipFile
+        from lxml import etree
+
+        with TemporaryDirectory('_txtz_output') as tdir:
+            # TXT
+            txt_name = 'index.txt'
+            if opts.txt_output_formatting.lower() == 'textile':
+                txt_name = 'index.text'
+            with TemporaryFile(txt_name) as tf:
+                TXTOutput.convert(self, oeb_book, tf, input_plugin, opts, log)
+                shutil.copy(tf, os.path.join(tdir, txt_name))
+
+            # Images
+            for item in oeb_book.manifest:
+                if item.media_type in OEB_IMAGES:
+                    if hasattr(self.writer, 'images'):
+                        path = os.path.join(tdir, 'images')
+                        if item.href in self.writer.images:
+                            href = self.writer.images[item.href]
+                        else:
+                            continue
+                    else:
+                        path = os.path.join(tdir, os.path.dirname(item.href))
+                        href = os.path.basename(item.href)
+                    if not os.path.exists(path):
+                        os.makedirs(path)
+                    with open(os.path.join(path, href), 'wb') as imgf:
+                        imgf.write(item.data)
+
+            # Metadata
+            with open(os.path.join(tdir, 'metadata.opf'), 'wb') as mdataf:
+                mdataf.write(etree.tostring(oeb_book.metadata.to_opf1()))
+
+            txtz = ZipFile(output_path, 'w')
+            txtz.add_dir(tdir)
